9449d7f162d882b74852b6f711819ac2055719c3
Muiz Ather <sheikh.muiz1@gmail.com>
2023-08-18 21:13:13 +0500

    Added Main Menu Screen and Game Over Screen
    Added 2 scenes, Menu and GameOver. Go to build settings and add these scenes in this order
    Menu
    Game
    GameOver

---
+++ Assets/menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
